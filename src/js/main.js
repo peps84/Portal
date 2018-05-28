e46b50eba8fb12c6729be53e974bd28c6029948b
Sam pepper <sampepper@gmail.com>
2018-05-28 17:23:03 +0100

    Added content, added CSS Grids based layout in partial, added mixin partial, added sticky header with height control, added fonts and fonts partial

--- src/js/main.js
+++ src/js/main.js
@@ -6,5 +6,23 @@
  */
 
 (function(){
-	console.log('main.js loaded');
+
+	var scrollPoint = 40;
+  var myHeader = document.querySelector('header');
+
+  var headerSquish = function (e){
+    console.log(window.pageYOffset);
+
+    if(window.pageYOffset > scrollPoint){
+      window.requestAnimationFrame(function() {
+        myHeader.classList.add('squish');
+      });
+    }else if(window.pageYOffset <= scrollPoint){
+      window.requestAnimationFrame(function() {
+        myHeader.classList.remove('squish');
+      });
+    }
+  };
+
+  window.addEventListener('scroll', headerSquish);
 })();
